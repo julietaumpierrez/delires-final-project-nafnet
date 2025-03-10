f5e5afb1d66987e36a878b51091c1372428f36ea
Julieta Umpierrez <jutpm30@gmail.com>
2025-03-10 17:06:34 +0100

    changed divisions too

--- basicsr/models/archs/NAFNet_arch_changed_gate_arch.py
+++ basicsr/models/archs/NAFNet_arch_changed_gate_arch.py
@@ -31,12 +31,12 @@ class NAFBlock(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
         self.conv2 = nn.Conv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1, groups=dw_channel,
                                bias=True)
-        self.conv3 = nn.Conv2d(in_channels=dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv3 = nn.Conv2d(in_channels=dw_channel // 4, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
         
         # Simplified Channel Attention
         self.sca = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
-            nn.Conv2d(in_channels=dw_channel // 2, out_channels=dw_channel // 2, kernel_size=1, padding=0, stride=1,
+            nn.Conv2d(in_channels=dw_channel // 4, out_channels=dw_channel // 4, kernel_size=1, padding=0, stride=1,
                       groups=1, bias=True),
         )
 
@@ -45,7 +45,7 @@ class NAFBlock(nn.Module):
 
         ffn_channel = FFN_Expand * c
         self.conv4 = nn.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
-        self.conv5 = nn.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv5 = nn.Conv2d(in_channels=ffn_channel // 4, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
 
         self.norm1 = LayerNorm2d(c)
         self.norm2 = LayerNorm2d(c)
