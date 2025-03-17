3eca3057053dc0a1c22a3e72077c1a59d7548212
Julieta Umpierrez <jutpm30@gmail.com>
2025-03-17 08:59:41 +0100

    files to run 12 channels

--- basicsr/models/archs/NAFNet_arch_changed_gate_n_arch.py
+++ basicsr/models/archs/NAFNet_arch_changed_gate_n_arch.py
@@ -21,8 +21,8 @@ from basicsr.models.archs.local_arch import Local_Base
 
 class SimpleGate(nn.Module):
     def forward(self, x):
-        x1 = x[:,:15,:,:]
-        x2 = x[:,15:30,:,:]
+        x1 = x[:,:12,:,:]
+        x2 = x[:,12:24,:,:]
         return x1 * x2
 
 class NAFBlock(nn.Module):
@@ -32,12 +32,12 @@ class NAFBlock(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
         self.conv2 = nn.Conv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1, groups=dw_channel,
                                bias=True)
-        self.conv3 = nn.Conv2d(in_channels=15, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv3 = nn.Conv2d(in_channels=12, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
         
         # Simplified Channel Attention
         self.sca = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
-            nn.Conv2d(in_channels=15, out_channels=15, kernel_size=1, padding=0, stride=1,
+            nn.Conv2d(in_channels=12, out_channels=12, kernel_size=1, padding=0, stride=1,
                       groups=1, bias=True),
         )
 
@@ -46,7 +46,7 @@ class NAFBlock(nn.Module):
 
         ffn_channel = FFN_Expand * c
         self.conv4 = nn.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
-        self.conv5 = nn.Conv2d(in_channels=15, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv5 = nn.Conv2d(in_channels=12, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
 
         self.norm1 = LayerNorm2d(c)
         self.norm2 = LayerNorm2d(c)
